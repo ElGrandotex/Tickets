39b1472ca968b40699252a98abb33fd507a7154d
ElGrandotex <jose.verdezoto.moran@gmail.com>
2025-03-03 22:24:56 -0500

    Update UserRepository

--- src/main/java/com/jv/Tickets/Auth/User/UserRepository.java
+++ src/main/java/com/jv/Tickets/Auth/User/UserRepository.java
@@ -1,9 +1,6 @@
 package com.jv.Tickets.Auth.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -12,10 +9,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByUsername(String username);
-
-    @Modifying()
-    @Query("update User u set u.firstname=:firstname, u.lastname=:lastname, u.phone=:phone where u.id = :id")
-    void updateUser(@Param(value = "id") Integer id, @Param(value = "firstname") String firstname, @Param(value = "lastname") String lastname , @Param(value = "phone") String phone);
-
 
 }
